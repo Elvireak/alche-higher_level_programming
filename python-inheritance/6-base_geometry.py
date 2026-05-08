8939cb81beb36f7b976638586a3695e38e0f7931
Elvire Akayezu <e.akayezu@alustudent.com>
2026-05-08 06:14:33 -0700

    Added the 6th file to the repo.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raises an Exception since area is not implemented."""
+        raise Exception("area() is not implemented")
