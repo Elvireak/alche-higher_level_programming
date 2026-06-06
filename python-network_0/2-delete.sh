69e99710d2a58875e6f82b44d7f505c9cad86124
unknown <e.akayezu@alustudent.com>
2026-06-06 15:37:47 +0400

    Fixed the file

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request to the URL passed as first argument
-curl -s -X DELETE "$1"
+# a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s "$1" -X DELETE
