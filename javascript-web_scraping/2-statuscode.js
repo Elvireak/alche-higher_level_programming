40d3f04df2cf12561ba2717009305a8d65efc9ab
Elvire Akayezu <e.akayezu@alustudent.com>
2026-08-02 03:19:48 -0700

    Re collected the error in this 2nd file.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-  console.log(`code: ${response.statusCode}`);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
