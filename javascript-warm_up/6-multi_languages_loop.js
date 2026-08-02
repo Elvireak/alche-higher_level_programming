66ec72e104d1c414a36cc3d2b52a6c59dc2d0514
Elvire Akayezu <e.akayezu@alustudent.com>
2026-08-02 03:50:01 -0700

    Added the 6th file to the repo.

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = [];
+
+for (const language of languages) {
+  lines.push(language);
+}
+
+console.log(lines.join('\n'));
